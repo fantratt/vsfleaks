44374b3090e43260b0ec21404779c1becf5796d8
kristian <tkman85@gmail.com>
2014-10-23 15:18:58 +0200

    overloaded some more static files for zinnia

--- vsfleakssite/forms.py
+++ vsfleakssite/forms.py
@@ -6,6 +6,7 @@ from django.utils.translation import ugettext as _
     
 class ReportForm(forms.ModelForm):
     phone = forms.RegexField(regex=r'^\+?1?\d{9,15}$', 
+                             label = _('phone'),
                              error_message = _("Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."),
                              required=False)
     class Meta:
